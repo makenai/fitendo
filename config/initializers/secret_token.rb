dd595c9bff16d3e2aeccdf3f6bd1a7dc6cb26cc8
Pawel Szymczykowski <makenai@gmail.com>
2013-02-23 09:41:56 -0800

    Code base initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fitendo::Application.config.secret_token = '988c2e0cd3bb0ee78a022bac8433f92bfe841d1e899cdb0ae60b4f407ae140a5a9cca56a2cc63fc87223e4e72c74377cbfc22ecdd7b31553f5e3671ba5e316f1'
