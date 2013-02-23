f659080799b5b6dbe91edd9fd0d51426238ff2fc
Pawel Szymczykowski <makenai@gmail.com>
2013-02-23 15:19:34 -0800

    Added production key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   def require_auth
     if session[:access_token] && session[:access_secret]
       @fitbit = Fitgem::Client.new(
-        :consumer_key    => 'e197c4fc4e8f49159716bcaa3af4756d',
-        :consumer_secret => '3147cf9465d1466b85ef209e81ea8ca4',
+        :consumer_key    => FITBIT_KEY,
+        :consumer_secret => FITBIT_SECRET,
         :token           => session[:access_token],
         :secret          => session[:access_secret]
       )
--- config/initializers/fitbit.rb
+++ config/initializers/fitbit.rb
@@ -1,4 +1,12 @@
+if Rails.env.production?
+  FITBIT_KEY    = 'a77db83c38904ee3b645f66da94c672b'
+  FITBIT_SECRET = '81fb20ee86474388b9fd0d89029f857c'
+else
+  FITBIT_KEY    = 'e197c4fc4e8f49159716bcaa3af4756d'
+  FITBIT_SECRET = '3147cf9465d1466b85ef209e81ea8ca4'
+end
+
 $fitbit = Fitgem::Client.new(
-  :consumer_key => 'e197c4fc4e8f49159716bcaa3af4756d',
-  :consumer_secret => '3147cf9465d1466b85ef209e81ea8ca4'
+  :consumer_key => FITBIT_KEY,
+  :consumer_secret => FITBIT_SECRET
 )
