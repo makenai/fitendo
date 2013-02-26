0b9b35e7918bdf865ec4e72bb70c63117e2e83a3
Pawel Szymczykowski <makenai@gmail.com>
2013-02-26 00:06:19 -0800

    debug

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -8,6 +8,7 @@ class ResultsController < ApplicationController
       Activity.update_activity!( @fitbit, @user, @date )
       redirect_to "/results?date=#{@date.strftime('%Y-%m-%d')}"
     end
+    p @user
     @previous_day = Activity.previous_day( @user['encodedId'], @date )
     @next_day = Activity.next_day( @user['encodedId'], @date )
     @activities = Activity.for_user( @fitbit, @user, @date )
